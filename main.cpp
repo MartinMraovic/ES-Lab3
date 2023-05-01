4076eb68c445a118bde908bd107dca432f87daad
MartinMraovic <Martin.Mraovic@students.ittralee.ie>
2023-05-01 21:11:03 +0000

    Final commit with comments
    Signed-off-by: MartinMraovic <Martin.Mraovic@students.ittralee.ie>

--- main.cpp
+++ main.cpp
@@ -1,28 +1,29 @@
 #include "mbed.h"
-#include "MMA7660.h"
-#include <cmath>
+#include "MMA7660.h" // include header file for MMA7660 accelerometer
+#include <cmath> // include math library for using sqrt and atan functions
 
-#define PI 3.141592654
+#define PI 3.141592654 // define the value of PI
 
-MMA7660 MMA(p28, p27);
-DigitalOut connectionLed(LED1);
+MMA7660 MMA(p28, p27); // create an object of MMA7660 class and initialize it with pin numbers
+DigitalOut connectionLed(LED1); // create an object of DigitalOut class for LED1
 
  
+// function to calculate the angle of the accelerometer data
 float calculateAngle(float x, float y, float z){
-    float angle = 0;
-    float bott_part = sqrt((y*y)+(z*z));
-    angle = atan(x/bott_part);
-    angle = (angle * 180.0)/PI;
-    return angle;   
-
+    float angle = 0; // initialize the angle variable to 0
+    float bott_part = sqrt((y*y)+(z*z)); // calculate the square root of y^2 + z^2
+    angle = atan(x/bott_part); // calculate the arctangent of x / bott_part
+    angle = (angle * 180.0)/PI; // convert the angle to degrees from radians
+    return angle; // return the calculated angle   
 } 
 
+// main function
 int main() {
-    if (MMA.testConnection())
-        connectionLed = 1;
+    if (MMA.testConnection()) // test if the MMA7660 accelerometer is connected
+        connectionLed = 1; // if connected, turn on the LED1
 
-while(1) {
-    printf("angle is %f \r\n" , calculateAngle(MMA.x(), MMA.y(), MMA.z()));
-    wait(1);
-  }
+    while(1) { // loop forever
+        printf("angle is %f \r\n" , calculateAngle(MMA.x(), MMA.y(), MMA.z())); // print the angle calculated from x, y, and z accelerometer data
+        wait(1); // wait 1 second
+    }
 }

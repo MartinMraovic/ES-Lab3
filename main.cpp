23df349ca7a191af736dd7b8d4dbd625d52f03c5
MartinMraovic <Martin.Mraovic@students.ittralee.ie>
2023-05-07 17:29:15 +0000

    Final CA Q12
    Signed-off-by: MartinMraovic <Martin.Mraovic@students.ittralee.ie>

--- main.cpp
+++ main.cpp
@@ -1,29 +1,50 @@
 #include "mbed.h"
-#include "MMA7660.h" // include header file for MMA7660 accelerometer
-#include <cmath> // include math library for using sqrt and atan functions
+#include "MMA7660.h"
 
-#define PI 3.141592654 // define the value of PI
+MMA7660 MMA(p28, p27);
 
-MMA7660 MMA(p28, p27); // create an object of MMA7660 class and initialize it with pin numbers
-DigitalOut connectionLed(LED1); // create an object of DigitalOut class for LED1
+// Define the PWM pins for controlling the LED colors
+PwmOut redLed(p23);
+PwmOut greenLed(p24);
+PwmOut blueLed(p25);
 
- 
-// function to calculate the angle of the accelerometer data
-float calculateAngle(float x, float y, float z){
-    float angle = 0; // initialize the angle variable to 0
-    float bott_part = sqrt((y*y)+(z*z)); // calculate the square root of y^2 + z^2
-    angle = atan(x/bott_part); // calculate the arctangent of x / bott_part
-    angle = (angle * 180.0)/PI; // convert the angle to degrees from radians
-    return angle; // return the calculated angle   
-} 
+// Function to map the accelerometer values to the LED brightness range
+float map(float value, float in_min, float in_max, float out_min, float out_max) {
+    return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
-// main function
 int main() {
-    if (MMA.testConnection()) // test if the MMA7660 accelerometer is connected
-        connectionLed = 1; // if connected, turn on the LED1
+    if (MMA.testConnection()) {
 
-    while(1) { // loop forever
-        printf("angle is %f \r\n" , calculateAngle(MMA.x(), MMA.y(), MMA.z())); // print the angle calculated from x, y, and z accelerometer data
-        wait(1); // wait 1 second
+        // Set the PWM period to 20ms
+        redLed.period(0.02);
+        greenLed.period(0.02);
+        blueLed.period(0.02);
+
+        while(1) {
+            // Read the accelerometer values
+            float x = MMA.x();
+            float y = MMA.y();
+            float z = MMA.z();
+
+            // Map the accelerometer values to the LED brightness range (0.0 to 1.0)
+            float redBrightness = map(fabs(x), 1.0, 0.0, 1.0, 0.0);
+            float greenBrightness = map(fabs(y), 1.0, 0.0, 1.0, 0.0);
+            float blueBrightness = map(fabs(z), 1.0, 0.0, 1.0, 0.0);
+
+            // Set the LED brightness
+            redLed.write(redBrightness);
+            greenLed.write(greenBrightness);
+            blueLed.write(blueBrightness);
+
+            // Print the axis values to the monitor
+            printf("X-axis: %f Y-axis: %f Z-axis: %f\r\n", x, y, z);
+
+            // Wait for 100ms before updating the values
+            wait(0.1);
+        }
+    }
+    else {
+        printf("MMA7660 not found\r\n");
     }
 }
